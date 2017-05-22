60ba0f8ba9e70edeaca55c1d3aef88a97e383f31
Wolf <wolf257@users.noreply.github.com>
2017-05-22 16:41:52 +0200

    edition directories + add TODO

--- myMod/directories.py
+++ myMod/directories.py
@@ -7,10 +7,21 @@ import imapclient, pyzmail, pprint, os, time
 #	TESTED
 #-----------------------------
 
+
 def create_directories(imapObj): #WORKS_TO_KEEP
     """ CREATES ALL THE DIRECTORIES existing IN the SERVERS """
 
     list = imapObj.list_folders()
+
+    #TODO : Create folder save_date,
+        # Put your self in it,
+        # Create file report (a)
+        # After each operation, write the content in (a)
+        # Close (a)
+
+    print("Sauvegarde effectuée le : " + time.strftime("%A") + ", " + time.strftime("%Y/%m/%d")
+    + " at : " + time.strftime("%H:%M:%S") )
+    print("Email concerné :" + "#email a mettre \n" )
 
     print("-------------- CREATION SUR VOTRE MACHINE DES DOSSIERS --------------")
 
