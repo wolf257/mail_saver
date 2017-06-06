cec91ab39104a3e6044cf5cca33d3a855433be27
Wolf <wolf257@users.noreply.github.com>
2017-06-06 15:15:50 +0200

    messages.py

---
+++ myMod/messages.py
@@ -0,0 +1,13 @@
+import imapclient, pyzmail, pprint, os, time
+from ./mails.py import *
+
+class message(object):
+    """docstring for message."""
+
+
+def create_message_file() :
+     #create file with right name
+
+def write_message() :
+    #open file with right name
+    #write
