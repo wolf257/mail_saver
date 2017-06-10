6066abce3a405e90829f856edd9912a23d05247b
Wolf <wolf257@users.noreply.github.com>
2017-06-10 21:41:51 +0200

    Choice terminal vs GUI for test

--- main.py
+++ main.py
@@ -13,12 +13,55 @@ import mymodules.server_directories as myserver_directories
 import mymodules.local_messages as mylocal_messages
 import mymodules.server_messages as myserver_messages
 
-imapObj = myconnections.connect()
+import interface
 
-#myserver_directories.print_all_folders(imapObj)
 
-#mylocal_directories.create_directories(imapObj)
+while 1 :
 
-#mymails.basic_process(imapObj)
+    #========================
+    # 1st choice
+    #========================
 
-myconnections.disconnect(imapObj)
+    a = input("How do you want me to work ? " + \
+        "\n 1 - via Terminal" + \
+        "\n 2 - via GUI" + \
+        "\n (enter) - Exit" + \
+        "\nYour choice : ")
+
+    #==================================
+    if a.strip() == '1' :
+
+        imapObj = myconnections.connect()
+
+        #========================
+        # 2nd choice
+        #========================
+
+        b = input("How do you want me to work ? " + \
+            "\n a - Basic process" + \
+            "\n b - Create directories"+
+            "\nYour choice : ")
+
+
+        if b.strip() == 'a' :
+            mymails.basic_process(imapObj)
+        elif b.strip == 'b' :
+            mylocal_directories.create_directories(imapObj)
+        else :
+            print("I dont know what to do. (restart)")
+
+        myconnections.disconnect(imapObj)
+
+    #==================================
+    elif a.strip() == '2' :
+        print("Go see the GUI")
+        interface.interface_tk()
+            # Note : program won't resume until you close the GUI
+
+    #==================================
+    elif a.strip() == '' :
+        break
+
+    #==================================
+    else :
+        print("I dont know what to do. (restart)")
